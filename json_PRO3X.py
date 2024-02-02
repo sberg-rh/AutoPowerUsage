7334d40d980f4182c84872907ff5e6955392e438
John Harrigan <jharriga@redhat.com>
2024-02-02 10:15:52 -0500

    Update json_PRO3X.py

--- json_PRO3X.py
+++ json_PRO3X.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-# Monitors power usage perf PDU Outlet using Redfish
+# Monitors power usage per PDU Outlet using Redfish
 # Writes JSON file in format ready for ingest in ElasticSearch
 #
 # Tested on RHEL 8.9 - Python 3.6.8
-# DEPENDENCIES: # python3 -m pip install ??
+# DEPENDENCIES: # python3 -m pip install pytz sushy
 #
 # File 'universal_resources.py' required
 # Redfish on PRO3X requires Firmmware Version 4.0.21 and later
@@ -16,7 +16,7 @@
 # DICTIONARY Format - dicts initialized in main()
 #
 # testrun_dict = {
-#     "start_ts": <curtime>,
+#     "start_ts": <start_curtime>,
 #     "test_type": "power-usage",
 #     "test_config": {
 #         testcfg_dict{}
@@ -25,25 +25,28 @@
 #         testres_dict{}
 #     },
 #     "test_summary": {
+#         "start_ts": <start_curtime>,
+#         "end_ts": <end_curtime>,
 #         "numsamples": <loopctr>,
-#         "total_rt": <total_rt:.1f>,
-#         "avg_pt": <avg_ptime:.2f>,
-#         "avg_pwr": <avg_reading:.2f>
+#         "total_rt": <total_rt>,
+#         "avg_pt": <avg_ptime>,
+#         "avg_pwr": <avg_reading>
 #     }
 # }
 ###########
 # testcfg_dict = {
-#     "pdu_ip": <pduIP>,
+#     "device_type": <dev_type>
+#     "device_ip": <dev_ip>,
 #     "interval": <interval>,
 #     "outlet": <outlet#>
 # }
 ###########
 # testres_dict = {
-#     "sample": <sample#>,
-#     "sample_data" = {
-#         "timestamp": <curdate>,
-#         "outlet": <outlet#>,
-#         "power": <wattage>
+#    "sample": <sample#>,
+#    "timestamp": <curdate>,
+#    "outlet": <outlet#>,
+#    "probetime": <ptime>,
+#    "power": <wattage>
 #     }
 # }
 
@@ -51,7 +54,9 @@
 ##############################################################
 # IMPORTS
 import argparse, logging, requests
-import sys, time, statistics, datetime, json, io
+import sys, time, statistics, json, io
+from datetime import datetime
+import pytz
 
 # adding flush=True' to ensure print() runs unbuffered mode
 import functools
@@ -72,7 +77,7 @@ from universal_resources import get_collection, get_resource
 def parseArgs(argv0):
     # Parse ARGs and return them
     parser = argparse.ArgumentParser(argv0, 
-            description = "Redfish PRO3X per Outlet Power Monitor")
+            description = "Redfish Outlet Power Monitor")
     req_grp = parser.add_argument_group(title='Required Optional')
     req_grp.add_argument('--ip', required=True, type=str, 
                          help='ip address (Redfish Server)')
@@ -80,6 +85,7 @@ def parseArgs(argv0):
                          help='delay in seconds between Readings')
     req_grp.add_argument('--outlet', required=True, type=str,
                          help='PDU outlet number to monitor')
+# Optional Args
     parser.add_argument('--user', type=str, 
                         help='user name (Redfish Server)', default="labuser")
     parser.add_argument('--passwd', type=str, 
@@ -88,8 +94,8 @@ def parseArgs(argv0):
 
     return theArgs
 
-def prepConn(the_args):
-    # Prepare the Redfish connection
+def prepConn(the_devIP, the_user, the_passwd):
+    # Prepare the Redfish connection to the device
 
     # Squelch insecure warnings - using basic auth
     requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
@@ -100,10 +106,10 @@ def prepConn(the_args):
     LOG.addHandler(logging.StreamHandler())
 
     # Verify Redfish Connection using basic authentication method
-    theConn = sushy.auth.BasicAuth(username=the_args.user, 
-                                      password=the_args.passwd)
+    theConn = sushy.auth.BasicAuth(username=the_user, 
+                                      password=the_passwd)
     try:
-        theRoot = sushy.Sushy(f'https://{the_args.ip}/redfish/v1',
+        theRoot = sushy.Sushy(f'https://{the_devIP}/redfish/v1',
                                auth=theConn, verify=False)
     except (sushy.exceptions.ConnectionError) as e:
         print("Connection Error, exiting...")
@@ -134,32 +140,35 @@ def init_rundict(the_curtime, the_type):
 
     return the_rundict
 
-def init_cfgdict(the_pduIP, the_interval, the_outlet):
+def init_cfgdict(the_devtype, the_devIP, the_interval, the_outlet):
     # Initialize new dict{} for the test Config
     the_cfgdict = {}             # new empty dict
 
-    the_cfgdict["pdu_ip"] = str(the_pduIP)
+    the_cfgdict["device_type"] = str(the_devtype)
+    the_cfgdict["device_ip"] = str(the_devIP)
     the_cfgdict["interval"] = str(the_interval)
     the_cfgdict["outlet"] = str(the_outlet)
 
     return the_cfgdict
 
-##def init_resdict(the_sample, the_ts, the_outlet, the_power):
-def init_samdict(the_ts, the_outlet, the_power):
-    # Initialize new dict{} for the test Results
-    the_samdict = {}             # new empty dict
+def init_resdict(the_sample, the_ts, the_outlet, the_power, the_ptime):
+    # Initialize new dict{} for this Sample's test Results
+    this_dict = {}             # new empty dict
 
-##    the_resdict["sample"] = int(the_sample)
-    the_samdict["timestamp"] = str(the_ts.strip())
-    the_samdict["outlet"] = str(the_outlet)
-    the_samdict["power"] = float(the_power)
+    this_dict["sample"] = int(the_sample)
+    this_dict["timestamp"] = str(the_ts.strip())
+    this_dict["outlet"] = str(the_outlet)
+    this_dict["power"] = float(the_power)
+    this_dict["probetime"] = float(the_ptime)
 
-    return the_samdict
+    return this_dict
 
-def init_sumdict(the_loopctr, the_runtime, the_avgPT, the_avgPWR):
+def init_sumdict(the_start, the_end, the_loopctr, the_runtime, the_avgPT, the_avgPWR):
     # Initialize new dict{} for the test Summary
     the_sumdict = {}             # new empty dict
 
+    the_sumdict["start_ts"] = str(the_start)
+    the_sumdict["end_ts"] = str(the_end)
     the_sumdict["numsamples"] = int(the_loopctr)
     the_sumdict["total_rt"] = float(the_runtime)
     the_sumdict["avg_ptime"] = float(the_avgPT)
@@ -169,7 +178,8 @@ def init_sumdict(the_loopctr, the_runtime, the_avgPT, the_avgPWR):
 
 def get_curtime():
     # Provides a consistent method for recording the current timestamp
-    the_time = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+##    the_time = datetime.now().strftime("%Y-%m-%dT%H:%M:%S-%Z")
+    the_time = datetime.now(tz=pytz.UTC).strftime("%Y-%m-%dT%H:%M:%S-%Z")
 
     return the_time
 
@@ -179,8 +189,7 @@ def main():
     # Dictionaries
     testrun_dict = {}        # complete testrun results
     testcfg_dict = {}        # test Configuration
-##    testres_dict = {}        # test Results (per Sample)
-    results_list = []        # All Sample results - list of testres_dict(s)
+    testres_list = []        # All Sample results - list of testres_dict(s)
 
     # Define VARs
     loopctr = 1                   # counts number of samples
@@ -188,6 +197,7 @@ def main():
     total_rt = 0                  # Total runtime (in sec)
     readings_list = []            # wattage readings
     ptimes_list = []              # probe timing results
+    dev_type = "PRO3X"            # hardcoded - only device type supported
     outlets_uri = "/redfish/v1/PowerEquipment/RackPDUs/1/Outlets"
     outlet_fields = {
         "id": base.Field('Id'),
@@ -197,24 +207,22 @@ def main():
 
     # Parse ARGs and define vars
     args = parseArgs(sys.argv[0])
-    pdu_ip = args.ip                        # user provided
-    pause_secs = args.interval              # user provided
-    outlet_number = args.outlet             # user provided
+    # Req'd args
+    dev_ip = args.ip                      # user provided
+    pause_secs = args.interval            # user provided
+    outlet_number = args.outlet           # Req'd for dev_type=PRO3X
+    # Optional args
+    dev_user = args.user                  # Over-rides default
+    dev_passwd = args.passwd              # Over-rides default
 
-    # Establish Connection using cmdline args
-    conn, root = prepConn(args)
-
+    # Establish Connection 
+    conn, root = prepConn(dev_ip, dev_user, dev_passwd)
     # Get the collection of Outlets
     outlets_coll = get_collection(root, outlets_uri)
-#    # Verify requested Outlet is available
-#    numOutlets = len(outlets_coll.get_members())   # SLOW
-#    if ( int(outlet_number) > int(numOutlets) ):
-#        print('Invalid Outlet Number requested, exiting\n')
-#        exit()
-
     theoutlet_uri = outlets_uri + '/' + outlet_number
 
-    # Record Start Time
+    ###################
+    # Record Monitoring Start Time
     start_curtime = get_curtime()
 
     # Basename for JSON output file
@@ -222,11 +230,11 @@ def main():
 
     # Initialize Test Run and Test Config dicts
     testrun_dict = init_rundict(get_curtime(), "power-usage")
-    testcfg_dict = init_cfgdict(pdu_ip, pause_secs, outlet_number)
+    testcfg_dict = init_cfgdict(dev_type, dev_ip, pause_secs, outlet_number)
     testrun_dict["test_config"] = testcfg_dict
 
     # Print opening message
-    print(f"Monitoring Outlet number: {outlet_number}. Pausing {pause_secs} seconds between readings")
+    print(f"Monitoring Outlet number: {outlet_number}. Pausing {pause_secs} seconds between Readings")
     print("Hit Control-C <SIGINT> to end\n")
 
     #######################################
@@ -245,25 +253,19 @@ def main():
             outlet_id = svr_outlet.id
             power_W = svr_outlet.power
             print(f'  Outlet# {outlet_id} Sample# {loopctr}')  # DEBUG
-##            print(f'    Timestamp:   {this_curtime}')
-##            print(f'    ProbeTime:   {this_ptime} sec')
-##            print(f'    Power:       {power_W} W\n')
 
             # Append to Summary stats for this Sample
             readings_list.append(float(power_W))
             ptimes_list.append(float(this_ptime))
 
             # Initialize Test Result dict for this Sample
-            # Per Sample (sample_dict) is nested within Test Result dict
             testres_dict = {}        # test Results (per Sample)
-            sample_dict = {}         # Sample data
+            testres_dict = init_resdict(loopctr, this_curtime, outlet_id,
+                                        power_W, this_ptime)
 
-            testres_dict["sample"] = int(loopctr)
-            sample_dict = init_samdict(this_curtime, outlet_id, power_W)
-            testres_dict["sample_data"] = sample_dict
-
-            # Append this Sample's Test Result dict to results_list
-            results_list.append(testres_dict)
+            # Append this Sample's Test Result dict to testres_list[]
+            # testres_list[] is a list of dicts, one per Sample
+            testres_list.append(testres_dict)
 
             # Pause and incr loopctr
             time.sleep(int(pause_secs))
@@ -275,6 +277,7 @@ def main():
     #######################################
     # Monitoring loop Interrupted - cleanup and write JSON Output
     final_ts = time.perf_counter()
+    end_curtime = get_curtime()
     conn.close()
 
     # Calculate the Averages
@@ -282,20 +285,12 @@ def main():
     avg_reading = round(statistics.mean(readings_list), 2)
     total_rt = round((final_ts - begin_ts), 1)
 
-    # Print the Summary Report
-##    print('SUMMARY REPORT')
-##    print(f'  Number of samples: {loopctr}')
-##    print(f'  Sample interval(in sec): {pause_secs}')
-##    print(f'  Total runtime(in sec): {total_rt}')
-##    print(f'  Average Probe time(in sec): {avg_ptime}')
-##    print(f'  Average Reading(in Watts): {avg_reading}')
-
-    # Insert complete samples_dict{} into testrun_dict (final dict)
-##    testrun_dict["test_results"] = samples_dict
-    testrun_dict["test_results"] = results_list
+    # Insert complete testres_list (per Sample testres_dict{})
+    testrun_dict["test_results"] = testres_list
 
     # Initialize Test Summary dict
-    testsum_dict = init_sumdict(loopctr, total_rt, avg_ptime, avg_reading)
+    testsum_dict = init_sumdict(start_curtime, end_curtime, loopctr,
+                                total_rt, avg_ptime, avg_reading)
     testrun_dict["test_summary"] = testsum_dict
 
     # Write JSON output file
