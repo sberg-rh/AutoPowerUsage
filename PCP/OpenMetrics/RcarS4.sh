564e9ac829f4e7fdd3d26603c15d30f0f0a18e2b
Shawn Berg <sberg@redhat.com>
2024-11-07 18:41:01 -0600

    Fixed typo

--- PCP/OpenMetrics/RcarS4.sh
+++ PCP/OpenMetrics/RcarS4.sh
@@ -6,7 +6,7 @@
 source /var/lib/pcp/pmdas/openmetrics/config.d/auto_vars.cfg
 
 # Get Readings - R-Car S4
-ssh root@renesas-rcar-s4-sidekick-$rcar4_number.auto-eng.bost2.dc.redhat.com "stdbuf -oL powermeter -p /dev/i2c-1 | \
+ssh root@renesas-rcar-s4-sidekick-$rcar4_number.auto.eng.bost2.dc.redhat.com "stdbuf -oL powermeter -p /dev/i2c-1 | \
     awk -v str="$rcar4_metric" '$0~str {
         print("# HELP Rensas R-Car S4 watts")
         print("# TYPE hardware gauge")
