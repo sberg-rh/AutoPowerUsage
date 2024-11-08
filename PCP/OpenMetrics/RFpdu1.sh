c18095b723540d60b0df4ab5e4fd3ca6353fca4e
Shawn Berg <sberg@redhat.com>
2024-11-07 19:45:09 -0600

    Fixed PDU reading to read watts "Reading"

--- PCP/OpenMetrics/RFpdu1.sh
+++ PCP/OpenMetrics/RFpdu1.sh
@@ -7,7 +7,7 @@ source /var/lib/pcp/pmdas/openmetrics/config.d/auto_vars.cfg
 
 # Get Readings - PDU1
 curl -kfsS https://${RFpdu_user}:${RFpdu_passwd}@${RFpdu_ip1}${RFpdu_url1} | \
-jq | \
+jq '{PowerWatts: .PowerWatts}' | \
 awk -v str="$RFpdu_metric" '$0~str {
     print("# HELP RFpdus Redfish SmartPDU watts")
     print("# TYPE RFpdus gauge")
