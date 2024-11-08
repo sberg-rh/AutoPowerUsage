6f0e612e84a0404312d97661b672653f72fe536e
Shawn Berg <sberg@redhat.com>
2024-11-07 18:26:37 -0600

    Adding vars for sidekick

--- PCP/OpenMetrics/RcarS4.sh
+++ PCP/OpenMetrics/RcarS4.sh
@@ -6,9 +6,9 @@
 source /var/lib/pcp/pmdas/openmetrics/config.d/auto_vars.cfg
 
 # Get Readings - R-Car S4
-stdbuf -oL powermeter -p /dev/i2c-1 | \
+ssh root@renesas-rcar-s4-sidekick-$rcar4_number.auto-eng.bost2.dc.redhat.com "stdbuf -oL powermeter -p /dev/i2c-1 | \
     awk -v str="$rcar4_metric" '$0~str {
         print("# HELP Rensas R-Car S4 watts")
         print("# TYPE hardware gauge")
         printf("watts %.1f\n", $3)
-}'
+}'"
