83b5c42cc2aaeec51823856cb5670feb549e3c0b
Shawn Berg <sberg@redhat.com>
2024-11-21 09:26:05 -0600

    Renaming powermeter to autopowermeter

--- PCP/OpenMetrics/RcarS4.sh
+++ PCP/OpenMetrics/RcarS4.sh
@@ -6,7 +6,7 @@
 source /var/lib/pcp/pmdas/openmetrics/config.d/auto_vars.cfg
 
 # Get Readings - R-Car S4
-ssh sidekick "stdbuf -oL powermeter -p /dev/i2c-1 | \
+ssh sidekick "stdbuf -oL autopowermeter -p /dev/i2c-1 | \
     awk -v str="$rcar4_metric" '$0~str {
         print("# HELP Rensas R-Car S4 watts")
         print("# TYPE hardware gauge")
