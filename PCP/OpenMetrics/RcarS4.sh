f406ce5df72c446af22da4c4a70f3e244f1acb53
Shawn Berg <sberg@redhat.com>
2024-12-11 14:18:09 -0600

    Fixed Rcar4 script

--- PCP/OpenMetrics/RcarS4.sh
+++ PCP/OpenMetrics/RcarS4.sh
@@ -6,9 +6,9 @@
 source /var/lib/pcp/pmdas/openmetrics/config.d/auto_vars.cfg
 
 # Get Readings - R-Car S4
-ssh sidekick "stdbuf -oL autopowermeter -p /dev/i2c-1 | \
+ssh sidekick "stdbuf -oL autopowermeter -p /dev/i2c-1" | \
     awk -v str="$rcar4_metric" '$0~str {
-        print("# HELP Rensas R-Car S4 watts")
+        print("# HELP Renesas R-Car S4 watts")
         print("# TYPE hardware gauge")
         printf("watts %.1f\n", $11)
-}'"
+}'
